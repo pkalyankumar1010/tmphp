4c7d667fea243374caa151ac47c214deb2acab66
pkalyankumar1010 <pkalyankumar1010@gmail.com>
2025-02-25 23:20:08 +0530

    Hello

--- 5.php
+++ 5.php
@@ -3,7 +3,7 @@ session_set_cookie_params([
     'lifetime' => 0,
     'path' => '/',
     'domain' => '',
-    'secure' => true,  // Only allow over HTTPS
+    'secure' => false,  // Only allow over HTTPS
     'httponly' => true, // Prevent JavaScript access
     'samesite' => 'Strict'
 ]);
